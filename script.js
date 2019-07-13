93e11f5e99381ee1ff96a4c1f7be51ffef1cf8f6
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-07-13 13:16:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,468 @@
+let techkaro{
+    name:"techkaro",
+    shortDescription:"8 months web development program",
+    longDescription:"Program (coding skills for the under-served)",
+    duration:"8-months",
+    numberOfStudents:100,
+    noOfGirls:60,
+    noOfBoys:40,
+    feesPerMonth:500,
+    totalCourseFees:4000,
+    sponsor:"engro-vopak-terminal",
+    noOfEmployedStudents:60,
+    batch:2019,
+    staff: {
+        headTrainer{
+            name:"Sarah",
+            gender:"female",
+            expertise:"full-stack-developer",
+            email:"sarah@gmail.com",
+            contactNo:0333-555555,
+                githubId: "sarahsga",
+                    linkedInId: "sarahsga",
+                        qualification: "software-engineer"
+        }
+programManager{
+    name: "tooba",
+        gender: "female",
+            expertise: "full-stack-developer",
+                qualification: "software-engineer",
+                    email: "tooba@gmail.com",
+                        contactNo: 0333 - 22222,
+                            githubId: "tooba13",
+                                linkedInId: "tooba-13"
+}
+
+technicalAdvisor{
+    name = "mashood-rastgar",
+        proffesion: "web-developer",
+            employedAt: "sastaTicket",
+                gender: "male",
+                    qualification: "software-engineer",
+                        email: "mashood@gmail.com",
+                            contactNo: 0333 - 22222,
+                                githubId: "mashoodrastgar",
+                                    linkedInId: "MashoodRastgar"
+}
+
+tehNinjas[
+    {
+        name="NoreenNaz",
+        gender: "female",
+        expertise: "front-end-develpment",
+        mail: "noreen@gmail.com",
+        contactNo: 0333 - 22222,
+        githubId: "noreenNaz",
+        linkedInId: "noreenNaz"
+
+    }
+    {
+    name = "Ursila",
+        gender: "female",
+            expertise: "front-end-develpment",
+                mail: "ursila@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "ursila10",
+                            linkedInId: "ursila10",
+
+    }
+{
+    name = "Khair-un-Nisa",
+        gender: "female",
+            expertise: "front-end-develpment",
+                mail: "khair@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "khair-un-nisa",
+                            linkedInId: "khair-un-nisa",
+
+    }
+{
+    name = "ali-khan",
+        gender: "female",
+            expertise: "front-end-develpment",
+                mail: "ali@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "aliKhan",
+                            linkedInId: "aliKhan",
+
+    }
+{
+    name = "Ahmed",
+        gender: "male",
+            expertise: "front-end-develpment",
+                mail: "Ahmed@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "AHMED",
+                            linkedInId: "AHMED",
+
+    }
+{
+    name = "QariWalli-Ullah",
+        gender: "male",
+            expertise: "front-end-develpment",
+                mail: "mashood@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "mashoodrastgar",
+                            linkedInId: "MashoodRastgar",
+
+    }
+{
+    name = "Waqas",
+        gender: "male",
+            expertise: "front-end-develpment",
+                mail: "waqas@gmail.com",
+                    contactNo: 0333 - 22222,
+                        githubId: "Waqas1",
+                            linkedInId: "Waqas1",
+
+    }
+
+]
+    }
+}
+students: [
+    {
+        name: "aliza",
+        gender: "female",
+        age: 00,
+        isEmployed: false,
+        group: "a",
+        contactNumber: 0333 - 1111,
+        email: "aliza@gmail.com",
+        ambassador: "noreen-naz",
+        travelTime: "30mins",
+        distance: "17km",
+        qualification: "bachelors",
+        githubId: "aliza14"
+
+    }
+    
+    name: "armia",
+    gender: "female",
+    age: 00,
+    isEmployed: true,
+    group: "b",
+    contactNumber: 0333 - 1111,
+    email: "armia@gmail.com",
+    ambassador: "khair-un-nisa",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+    employmentDetails{
+        worksAt: "nasra-public-school",
+        profession: "teacher",
+        salary: xxxx
+    }
+
+}
+
+    name: "arifa",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "arifa@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "1.5hours",
+    distance: "25km",
+    qualification: "intermediate",
+
+}
+
+    name: "sufya",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "sufya@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "1.5hours",
+    distance: "25km",
+    qualification: "intermediate",
+
+}
+
+    name: "samreen",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "samreen@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "40mins",
+    distance: "19km",
+    qualification: "bachelors",
+
+}
+
+    name: "fahmeen",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "fahmeen@gmail.com",
+    ambassador: "ursila",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "sana",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "ursila",
+    travelTime: "1hour",
+    distance: "22km",
+    qualification: "bachelors",
+
+}
+
+    name: "farheen",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "farheen@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "40mins",
+    distance: "18km",
+    qualification: "bachelors",
+
+}
+
+    name: "jamila",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "jamila@gmail.com",
+    ambassador: "waqas",
+    travelTime: "20mins",
+    distance: "9km",
+    qualification: "bachelors",
+
+}
+
+    name: "khadija",
+    gender: "female",
+    age: 00,
+    isEmployed: true,
+    group: "b",
+    contactNumber: 0333 - 1111,
+    email: "khadija@gmail.com",
+    ambassador: "khair-un-nisa",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+    employment: {
+        worksAt: "abc-school",
+        salary: xxxx,
+        proffesion: "teacher"
+
+    }
+
+}
+
+    name: "somia",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "b",
+    contactNumber: 0333 - 1111,
+    email: "somia@gmail.com",
+    ambassador: "ali-khan",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+    name: "aliza",
+    gender: "female",
+    age: 00,
+    isEmployed: false,
+    group: "a",
+    contactNumber: 0333 - 1111,
+    email: "aliza@gmail.com",
+    ambassador: "noreen-naz",
+    travelTime: "30mins",
+    distance: "17km",
+    qualification: "bachelors",
+
+}
+
+]
+events[
+    {
+        name:"girls-take-charge",
+        eventManager:["tooba", "moezza"],
+        noOfparticipants:25,
+        companiesVisited:["planet-n", "unilever", "seed-venture"],
+        namesOfParticipants:["aliza","sufya", "armia", "nazia"]
+    }
+    {
+        name:"elevate",
+        eventManager:["tooba", "sadaffe-abid"],
+        noOfparticipants:10,
+    }
+    {
+        name:"eid-milan-party",
+        eventArrangedBy:"tech-karo",
+        noOfparticipants:100,
+
+    }
+]
+                
+}
